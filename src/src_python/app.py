fdf939f13cac0a3abff381d6e82ebe2b18c114f0
Erika-colla <collaguazobelen8@gmail.com>
2025-05-07 09:03:34 -0500

    Add graficas

--- src/src_python/app.py
+++ src/src_python/app.py
@@ -1,33 +1,51 @@
 from benchmarking import Benchmarking
 from metodosordenamiento import MetodoOrdenamiento
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     print("funciona")
 
-    metodos_obj = MetodoOrdenamiento()
+    metodos = MetodoOrdenamiento()
     benchmark = Benchmarking()
-    tam = 10000
-    arreglo_base = benchmark.build_arreglo(tam)
-    
-    metodos = {
-        "burbuja": metodos_obj.sort_bubble,
-        "seleccion": metodos_obj.sort_seleccion,
-    }
-    
+    tamanios = [500, 1000, 2000]
     resultados = []
-    for nombre, metodo in metodos.items():
-        tiempo = benchmark.medir_tiempo(metodo, arreglo_base)
-        tuplaResultado = (tam, nombre, tiempo)
-        resultados.append(tuplaResultado)
+
+    for tam in tamanios:
+        arreglo_base = benchmark.build_arreglo(tam)
+
+        metodos_dict = {
+            "burbuja": metodos.sort_bubble,
+            "seleccion": metodos.sort_seleccion,
+        }
+
+        for nombre, metodo in metodos_dict.items():
+            tiempo = benchmark.medir_tiempo(metodo, arreglo_base)  
+            tuplaResultado = (tam, nombre, tiempo)
+            resultados.append(tuplaResultado)
 
     for resultado in resultados:
         tam, nombre, tiempo = resultado
         print(f"Tamaño: {tam}, Método: {nombre}, Tiempo: {tiempo:.6f} segundos")
 
-    #a =5
-    #b=a
-    #print(a)
-    #print (b)
-    #b= 2 *a
-    #print(a)
-    #print (b) variable metodos que se aun diccionario dos elemento s burbuja y seleccion
+    tiempos_by_metodo = {
+        "burbuja": [],
+        "seleccion": [],
+    }
+
+    for tam, nombre, tiempo in resultados:
+        tiempos_by_metodo[nombre].append(tiempo)
+
+    plt.figure(figsize=(10, 6))
+
+    for nombre, tiempos in tiempos_by_metodo.items():
+        plt.plot(tamanios, tiempos, label=nombre, marker='o')
+
+    plt.title("Camparativa metodos \n Erika Collaguazo -2025-07-05 9:0 0am")
+    plt.get_current_fig_manager().set_window_title("Comprativa métodos" \
+    "Erika Collaguazo - 2025-07-05- 9:00 am")
+    
+    plt.xlabel("Datos eje X")
+    plt.ylabel("Datos eje Y")
+    plt.legend()
+   
+    plt.show()
